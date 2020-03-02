71a33097ce19be520872e7aeb460dc0603cbc3a0
kuetherb <kuetherb@oregonstate.edu>
2020-03-01 16:14:42 -0800

    commit 4

--- task.py
+++ task.py
@@ -1,2 +1,8 @@
+import math
+
 def firstrun():
     return "success"
+
+def circle_area(radius):
+    area = math.pi * (radius * radius)
+    return area
--- tests.py
+++ tests.py
@@ -1,5 +1,6 @@
 import unittest
 import task
+import math
 
 
 class TestCase(unittest.TestCase):
@@ -11,6 +12,11 @@ class TestCase(unittest.TestCase):
     def test2(self):
         expected = "failure"
         self.assertNotEqual(expected, task.firstrun())
+
+    def test3(self):
+        radius = 10
+        expected = 314
+        self.assertEqual(expected, task.circle_area(radius))
 
 
 if __name__ == '__main__':
